93210dc85c5889c0666a1fb5349e4433f5e546dc
SvitLana1990 <svitlana112@outlook.es>
2023-11-19 09:20:18 +0100

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,20 +12,6 @@ export const App = () => {
   const [contacts, setContacts] = useState(JSON.parse(savedContacts) || []);
   const [filter, setFilter] = useState('');
 
-  const updateFilter = newFilter => {
-    setFilter(newFilter);
-  };
-
-  const deleteItem = itemId => {
-    setContacts(prevContacts =>
-      prevContacts.filter(item => item.id !== itemId)
-    );
-  };
-
-  useEffect(() => {
-    window.localStorage.setItem(storageKey, JSON.stringify(contacts));
-  }, [contacts]);
-
   const addItem = newContact => {
     if (
       contacts.some(
@@ -38,10 +24,24 @@ export const App = () => {
     }
   };
 
+  const updateFilter = newFilter => {
+    setFilter(newFilter);
+  };
   const filteredItem = contacts.filter(item => {
     const hasName = item.name.toLowerCase().includes(filter.toLowerCase());
     return hasName;
   });
+
+  const deleteItem = itemId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(item => item.id !== itemId)
+    );
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem(storageKey, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <Container>
       <Title>Phonebook</Title>
